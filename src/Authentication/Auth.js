0033859797192ce030d8f2067678e1f53cae022e
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-11-24 17:26:42 -0200

    Adds Authtentications component
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

---
+++ src/Authentication/Auth.js
@@ -0,0 +1,15 @@
+export default class Auth {
+    static login(){
+        localStorage.setItem('login', 1);
+    }
+    static logged(){
+        if (localStorage.getItem('login') == 1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    static logout(){
+        localStorage.setItem('login', 0);
+    }
+}
