eacd58c2f92a8561876fc196a9b7e0951b283778
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-11-24 17:26:22 -0200

    Adds Test page
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- src/Screens/Principal.js
+++ src/Screens/Principal.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 
 
-class Principal extends Component {
+export default class Principal extends Component {
     render(){
+        return (
         <div>
             <Typography> Teste </Typography>
 
         </div>
+        )
     }
 }
