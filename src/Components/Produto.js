ca8a128ad22b2c91945a82e274f2c897641050fd
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-11-27 21:43:34 -0200

    Adds product details.

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import { CardContent, CardMedia, Button } from '@material-ui/core';
+import { CardContent, CardMedia, Button, Grid } from '@material-ui/core';
 import filtro_barro from '../Assets/filtro_barro.jpg'
 import CartService from '../Services/CartService';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
-
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import ProductDetail from './ProductDetail';
+import CustomizedModal from './CustomizedModal';
 
 export default class Produto extends Component {
     constructor(props){
         super(props);
         this.state = {
-            nome: this.props.nome,
-            preco: this.props.preco,
-            img: this.props.img,
-        }
 
+            open: false,
+            json: this.props.json
+        }
+        console.log('json no produto = ', this.state.json);
         this.sendToCart = this.sendToCart.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
+
+    openModal() {
+        this.setState({ open: true });
+    };
+    closeModal() {
+        this.setState({ open: false });
+    };
 
     sendToCart(state){
         console.log("SEND TO CART");
         let json = {
-            nome: this.state.nome,
-            preco: this.state.preco,
-            img: this.state.img
+            nome: this.state.json.nome,
+            preco: this.state.json.preco,
+            img: this.state.json.img
         }
         console.log('json = ', json)
         CartService.addCart(json);
@@ -50,20 +61,34 @@ export default class Produto extends Component {
         return(
             <Card >
                 <CardContent>
-                    <CardMedia style={styles.image} image={this.getImage(this.state.img)} title="Teste" >
+                    <CardMedia style={styles.image} image={this.getImage(this.state.json.img)} title="Teste" >
                         {/* <img alt="nao tem" src={require('../Assets/filtro_barro.jpg')} /> ESSA APARECE */} 
                         {/* <img alt="nao tem" width="20" height="20" src={'../Assets/filtro_barro.jpg'} /> */}
                     </CardMedia>
                     <Typography variant="body1">
-                        {this.state.nome}
+                        {this.state.json.nome}
                     </Typography>
                     <Typography variant="h6">
-                        R$: {this.state.preco},00
+                        R$: {this.state.json.preco},00
                     </Typography>
-                    <Button variant="contained" color="secondary" size="small" >Comprar</Button>
-                    <IconButton variant="fab" color="secondary" aria-label="Add" size="small" onClick={this.sendToCart} >
-                        <AddIcon />
-                    </IconButton>
+                    
+                    <Grid container spacing={24}>
+                        <Grid item xs={6}>    
+                            <Button variant="contained" color="secondary" size="small" >Comprar</Button>
+                        </Grid>
+
+                        <Grid item xs={3}>
+                            <IconButton variant="extended" color="primary" aria-label="Add" size="small" onClick={this.sendToCart} >
+                                <ShoppingCart /><AddIcon />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                    
+                    <Button variant="contained" color="primary" size="small" onClick={this.openModal} >Detalhes</Button>
+
+                    <CustomizedModal open={this.state.open} onClose={this.closeModal}>
+                        <ProductDetail json={this.state.json} />
+                    </CustomizedModal>
                 </CardContent>
             </Card >
         )
