2e64720af70485855b931cbd5064c231cb826d29
joaovitorml <joao.lemos4000@gmail.com>
2018-11-29 01:22:00 -0200

    Finalizada a página de ajuda.

--- src/Screens/Ajuda.js
+++ src/Screens/Ajuda.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { CardContent, CardMedia, Button, Grid } from '@material-ui/core';
+import CustomizedModal from '../Components/CustomizedModal';
 
 export default class Ajuda extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false,
+      json: this.props.json
+    }
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+      this.setState({ open: true });
+  }
+  closeModal(){
+    this.setState({open: false});
+  }
     render(){
         return (
         <div>
@@ -10,7 +28,12 @@ export default class Ajuda extends Component {
             <textarea cols="60" rows="10"></textarea>
             <br/>
             <br/>
-            <button>Enviar</button>
+            <Button variant="contained" color="primary" onClick={this.openModal}>Enviar</Button>
+            <CustomizedModal open={this.state.open} onClose={this.closeModal}>
+                <Typography variant="h4" >Mensagem enviada para nossa caixa de correios.
+                Por favor, aguarde o retorno</Typography>
+                <Button variant="contained" color="primary" onClick={this.closeModal}>Ok</Button>
+            </CustomizedModal>
           </fieldset>
         </div>
         )
