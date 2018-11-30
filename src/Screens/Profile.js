3fcdc62b665d17787991adc17462c54455d0a047
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-11-30 16:24:44 -0200

    Adds adress and edit profile button to Profile Page.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -4,6 +4,7 @@ import {
     CardContent,
     Typography,
     Grid,
+    Button,
 } from '@material-ui/core';
 import Auth from '../Authentication/Auth';
 
@@ -22,39 +23,50 @@ export default class Login extends Component {
     render(){
         return(
             <Card>
-                <Grid container spacing={24}>
-                        <CardContent>
+                <CardContent>
+                    <Grid container direction="row">
+                        <Grid item xs={9}>
                             <Typography variant="h4">João Genérico da Silva</Typography>
-                            <br/>
-                            <Typography variant="h6"><b>Cidade:</b> Gama, DF</Typography>
-                            <Typography variant="h7"><b>E-mail:</b> macahaogod@gmail.com</Typography>
-                            <br/>
-                            <Typography variant="h5">Descrição</Typography>
-                            <br/>
-                            <Typography variant="p">Um dia eu tive a brilhante
-                            ideia de fazer uma excursão no deserto. Numa noite,
-                            minha caravana estava toda lá quando fui dormir, porém, quando
-                            abri meus olhos, todos tinham desaparecido, como se tivessem
-                            sido levados pelas areias do deserto. E junto com eles foram-se mapa,
-                            alimentos, roupas, camelos, e principalmente, a água. Eu já estava
-                            sentido a sede me invadir.</Typography>
-                            <br/>
-                            <Typography variant="p">Comecei a me desesperar, correr em
-                            uma direção só, na esperança de um milagre. Quando comecei a sentir os
-                            sintomas da desidratação e perceber que esse deserto seria meu leito
-                            de morte, eu me deparo com um celular quase que completamente coberto pela
-                            areia. Ele estava aberto em uma página, a página deste incrível site.
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography align="right">
+                            <Button variant="contained" size="small" color="primary" >Editar Perfil</Button>
                             </Typography>
-                            <br/>
-                            <Typography variant="p">Logo
-                            vi que tinha uma promoção imperdível de um filtro de barro que eu sempre
-                            quis, nao podia esperar, encomendei na hora mesmo. E em pouco tempo saciei minha
-                            sede com o belíssimo produto que comprei. Portanto, posso dizer que esse site
-                            salvou minha vida.
-                            </Typography>
-
-                        </CardContent>
-                </Grid>
+                        </Grid>
+                    </Grid>
+                    <br/>
+                    <br/>
+                    <Typography variant="h7"><b>E-mail:</b> makahaogod@gmail.com</Typography>
+                    <br/>
+                    <Typography variant="h5">Descrição</Typography>
+                    <br/>
+                    <Typography variant="p">Um dia eu tive a brilhante
+                    ideia de fazer uma excursão no deserto. Numa noite,
+                    minha caravana estava toda lá quando fui dormir, porém, quando
+                    abri meus olhos, todos tinham desaparecido, como se tivessem
+                    sido levados pelas areias do deserto. E junto com eles foram-se mapa,
+                    alimentos, roupas, camelos, e principalmente, a água. Eu já estava
+                    sentido a sede me invadir.</Typography>
+                    <br/>
+                    <Typography variant="p">Comecei a me desesperar, correr em
+                    uma direção só, na esperança de um milagre. Quando comecei a sentir os
+                    sintomas da desidratação e perceber que esse deserto seria meu leito
+                    de morte, eu me deparo com um celular quase que completamente coberto pela
+                    areia. Ele estava aberto em uma página, a página deste incrível site.
+                    </Typography>
+                    <br/>
+                    <Typography variant="p">Logo
+                    vi que tinha uma promoção imperdível de um filtro de barro que eu sempre
+                    quis, nao podia esperar, encomendei na hora mesmo. E em pouco tempo saciei minha
+                    sede com o belíssimo produto que comprei. Portanto, posso dizer que esse site
+                    salvou minha vida.
+                    </Typography>
+                    <br/>
+                    <Typography variant="h5">Endereço: </Typography>
+                    <Typography variant="body1">CEP: 711000-000</Typography>
+                    <Typography variant="body1">Cidade: Brasilia</Typography>
+                    <Typography variant="body1">Bairro: Gama</Typography>
+                </CardContent>
             </Card>
         )
     }
