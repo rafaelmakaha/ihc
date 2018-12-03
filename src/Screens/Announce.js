434b4644f89daab9e83afce6904171ee00d262d3
joaovitorml <joao.lemos4000@gmail.com>
2018-12-03 18:39:24 -0200

    Página de anunciar implementada.

--- src/Screens/Announce.js
+++ src/Screens/Announce.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { Button, Input, FormLabel } from '@material-ui/core';
+import { Button, Input, FormLabel, FormControlLabel, TextField, RadioGroup, Radio } from '@material-ui/core';
 import CustomizedModal from '../Components/CustomizedModal';
 import { Link } from 'react-router-dom';
 
@@ -9,7 +9,9 @@ export default class MyAnnounces extends Component {
     super(props);
     this.state = {
       open: false,
-      json: this.props.json
+      json: this.props.json,
+      value: 'filtros',
+      value2: 'novo'
     }
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
@@ -22,19 +24,38 @@ export default class MyAnnounces extends Component {
     window.location.reload();
   }
 
-  getValue(){
-    var testando = this.refs.teste.getValue;
-    console.log(testando);
-  }
+  handleChange = event => {
+      this.setState({ value: event.target.value });
+    };
+  handleChange2 = event => {
+        this.setState({ value2: event.target.value2 });
+      };
 
   render(){
       return (
       <div>
         <fieldset><legend>Informações do produto</legend>
           <p><FormLabel>Nome do produto: &nbsp; <Input type="text" size="40"/></FormLabel></p>
-          <p><FormLabel>Tipo do produto: <Input type="text" size="40"/></FormLabel></p>
-          <p><FormLabel>Local: <Input ref="teste" type="text" size="40"/></FormLabel></p>
-          <p><FormLabel>Descrição do produto: </FormLabel></p>
+          <p><FormLabel>Tipo do produto:
+          <RadioGroup value={this.state.value}
+            onChange={this.handleChange}>
+          <FormControlLabel value='filtros' control={<Radio color="primary"/>} label="Filtro de barro"/>
+          <FormControlLabel value='purificador' control={<Radio color="primary"/>} label="Purificador"/>
+          <FormControlLabel value='ozonizadores' control={<Radio color="primary"/>} label="Ozonizador"/>
+          <FormControlLabel value='bebedouros de galão' control={<Radio color="primary"/>} label="Bebedouro de galão"/>
+          <FormControlLabel value='bebedouros de pressão' control={<Radio color="primary"/>} label="Bebedouro de pressão"/>
+          </RadioGroup>
+          </FormLabel></p>
+          <p><FormLabel>Localização: <Input ref="teste" type="text" size="40"/></FormLabel></p>
+          <p><FormLabel>Preço: R$ <TextField type="number"/></FormLabel></p>
+          <p><FormLabel>Classificação:
+          <RadioGroup value={this.state.value2}
+            onChange={this.handleChange2}>
+          <FormControlLabel value='novo' control={<Radio color="primary"/>} label="Novo"/>
+          <FormControlLabel value='usado' control={<Radio color="primary"/>} label="Usado"/>
+          </RadioGroup>
+          </FormLabel></p>
+          <p><FormLabel>Descrição: </FormLabel></p>
           <textarea cols="60" rows="10"></textarea>
           <br/>
           <br/>
