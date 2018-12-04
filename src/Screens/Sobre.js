3e3c4149eac2429f8952bc9aafe280db7f26b4e6
Rafael Makaha <rafael.makaha@gmail.com>
2018-12-03 22:04:18 -0200

    Changes Fonts on AboutPage.

--- src/Screens/Sobre.js
+++ src/Screens/Sobre.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
     Typography,
+    List,
+    ListItem
 } from '@material-ui/core';
 
 export default class Sobre extends Component {
@@ -9,27 +11,34 @@ export default class Sobre extends Component {
           <div>
             <Typography variant="h3">Surgimento da empresa</Typography>
             <br/>
-            <Typography variant="p">Nossa empresa surgiu em meados de 2018, quando um de nossos
+            <Typography variant="h6">Nossa empresa surgiu em meados de 2018, quando um de nossos
             fundadores, o gênio Rafael Makaha
             pensou em realizarmos um site de venda de filtros e bebedouros de qualidade inimaginável. Essa
             ideia foi prontamente aceita pelos outros dois sócios: João Vítor Morandi e Rômulo Souza.</Typography>
 
-            <Typography variant="p">No início, surgiram várias dificuldades, mas com a ajuda de investidores e com a ampla
+            <Typography variant="h6">No início, surgiram várias dificuldades, mas com a ajuda de investidores e com a ampla
             divulgação promovida com a apresentação do site na matéria de Interação Humano-Computador (IHC), a
             empresa começou a tomar forma, e as pessoas, sedentas para adquirir filtros e bebedouros incríveis
             e maravilhosos, começaram a entrar em contato.</Typography>
 
-            <Typography variant="p">Em pouco tempo, a FGAcqua começou a crescer exponencialmente, se tornando a empresa
+            <Typography variant="h6">Em pouco tempo, a FGAcqua começou a crescer exponencialmente, se tornando a empresa
             multinacional que conhecemos hoje. Então não se esqueça, se tiver um sonho, por mais absurdo que
             seja, nunca desista, que um dia ele pode se tornar realidade.</Typography>
+            
+            <br/>
+            <Typography variant="h3">Membros</Typography>
 
-            <h2>Membros</h2>
-
-            <ul>
-              <li>João Vítor Morandi - Sócio</li>
-              <li>Rafael Makaha - Sócio</li>
-              <li>Rômulo Vinícius - Sócio</li>
-            </ul>
+            <List>
+                <ListItem>
+                    <Typography variant="h6">João Vítor Morandi - Sócio</Typography>
+                </ListItem>
+                <ListItem>
+                    <Typography variant="h6">Rafael Makaha - Sócio</Typography>
+                </ListItem>
+                <ListItem>
+                    <Typography variant="h6">Rômulo Vinícius - Sócio</Typography>
+                </ListItem>
+            </List>
         </div>
         )
     }
